c4148789309b3ec1b77d9e38790d367c64600075
Ehren Murdick <emurdick@pivotal.io>
2016-01-06 20:41:38 -0800

    Add highlighting to active item; fixes #2
    Signed-off-by: Liam Morley <lmorley@pivotal.io>

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import 'moment-duration-format';
+import 'styles/Box.scss';
 
 class Box extends React.Component {
   formattedTime() {
@@ -9,10 +10,17 @@ class Box extends React.Component {
   handleActivation() {
     this.props.onActivate(this.props.id);
   }
+  boxComputedClassName() {
+    var classes = ['table-view-cell'];
+    if (this.props.active) {
+      classes.push('active');
+    }
+    return classes.join(' ');
+  }
   render() {
     let activeState = this.props.active ? 'Pause' : 'Start';
     return (
-      <li className="table-view-cell" onClick={this.handleActivation.bind(this)}>
+      <li className={this.boxComputedClassName()} onClick={this.handleActivation.bind(this)}>
         <h2>{this.props.name}</h2>
         {this.formattedTime()}
         <button className="activate btn">{activeState}</button>
--- test/components/BoxTest.js
+++ test/components/BoxTest.js
@@ -10,15 +10,32 @@ import Box from 'components/Box';
 
 describe('Box', () => {
   let activateSpy;
-  let box;
   beforeEach(() => {
     activateSpy = sinon.spy();
-    box = TestUtils.renderIntoDocument(<Box id={1} key={1} onActivate={activateSpy} />);
   });
+
+  function createBox(props={}) {
+    return TestUtils.renderIntoDocument(
+      <Box
+        id={props.id || 1}
+        key={props.key || 1}
+        active={props.active || false}
+        onActivate={props.onActivate || activateSpy}
+      />
+    );
+  }
+
   it('calls onActivate callback upon activation', () => {
+    let box = createBox();
     let activateLink = TestUtils.findRenderedDOMComponentWithClass(box, 'activate');
     TestUtils.Simulate.click(activateLink);
 
     expect(activateSpy).to.have.been.calledWith(1);
+  });
+
+  it('has an active class if its currently active', () => {
+    let box = createBox({ active: true });
+    let li = TestUtils.findRenderedDOMComponentWithTag(box, 'li');
+    expect(li.attributes.class.nodeValue).to.contain('active');
   });
 });
